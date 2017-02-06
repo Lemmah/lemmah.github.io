bab4c8445c918844fc6a9f11929f45e84ea7fd81
Lemmah <lemayiannakolah@gmail.com>
2017-02-06 05:01:51 +0300

    Major modifications
    Added a contact.php to send me emails
    Modified my modal to make it  form using the post method
    Added a .htaccess configuration file

---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php
+  $myEmailAddress="lemayiannakolah@gmail.com";
+  $subject="Email from Anything for Growth";
+
+  $name= $_POST['sname'];
+  $email= $_POST['semail'];
+  $message= $_POST['comment'];
+  $header= "from: $name <$email>";
+
+  mail($myEmailAddress, $subject, $message, $header);
+>
